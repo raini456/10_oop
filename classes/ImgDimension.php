e1d62f85f6271a03ca9d43e36ebc6bcdaabe4b51
pr04 <pr04@figd-1711704.figdtk.de>
2018-04-06 10:35:56 +0200

    2. Version 06.04. 10:30 h OOP Basics mit Dateiupload/Dateiverschieben und Dimensionsberechnung

--- classes/ImgDimension.php
+++ classes/ImgDimension.php
@@ -6,7 +6,7 @@ class ImgDimension {
   const FILENAME_RANDOM = 2;
   const FILENAME_NUM = 3;
   const FILENAME_AUTO = 0;
-  const DIMENSION_AUTO = 1;
+  const DIMENSION_AUTO = 0;
 
   private $srcFolder = '';
   private $srcFileNamePattern = '';
@@ -15,6 +15,7 @@ class ImgDimension {
   private $dstFileNameType = self::FILENAME_ORIGIN; //ImgDimension:: würde auch möglich sein, bzw. this->::;
   private $dstFileNamePrefix = '';
   private $srcW = '';
+  private $dstW = '';
   private $dstH = '';
   private $dstCompressionLevel = 75;
 
@@ -46,13 +47,35 @@ class ImgDimension {
     $this->dstFileNameType = $type;
     $this->dstFileNamePrefix = $prefix;
   }
-/**
- * es sollten an der Stelle nicht unbedingt Standardwerte eingegeben werden, es ist 
- * nicht unbedingt sinnvoll, hier Bilder standardmäßig zu formatieren
- **/
-  public function setDestDimensions(int $w, int $h) {
-    $this->srcW = $w;
+
+  /**
+   * es sollten an der Stelle nicht unbedingt Standardwerte eingegeben werden, es ist 
+   * nicht unbedingt sinnvoll, hier Bilder standardmäßig zu formatieren
+   * */
+  public function setDestDimensions(int $w = self::DIMENSION_AUTO, int $h = self::DIMENSION_AUTO) {
+    $this->dstW = $w;
     $this->dstH = $h;
+  }
+
+  private function calcDstDimensions(int $w, int $h, string $path) {
+    $info = getImageSize($path); //kreiert ein Array mit Breite: info[0] und Höhe info[1] 
+    if ($w > 0 && $h > 0) {
+      $this->dstW = $w;
+      $this->dstH = $h;
+    } elseif ($w === 0 && $h === 0) {
+      $this->dstW = $path[0];
+      $this->dstH = $path[1];
+    } elseif ($w > 0 && $h === 0) {
+      $this->dstW = $w;
+      $this->dstH = $this->calcDimension($info[0], $info[1], $w);
+    } else {
+      $this->dstH = $h;
+      $this->dstW = $this->calcDimension($info[1], $info[0], $w);
+    }
+  }
+
+  private function calcDimension($x1, $y1, $x2) {
+    return $y1 * $x2 / $x1;
   }
 
   public function setDstCompressionLevel(int $level) {
@@ -60,16 +83,15 @@ class ImgDimension {
   }
 
   public function execute() {
-    $srcPaths=$this->findFiles();
-    foreach($srcPaths as $srcPath){
-      $fileType=$this->getImageFileType($srcPath);
-//      printf($srcPath."<br>");
-      printf($fileType."<br>");      
+    $srcPaths = $this->findFiles();
+    foreach ($srcPaths as $srcPath) {
+      $fileType = $this->getImageFileType($srcPath);
+      $this->calcDstDimensions($this->dstW, $this->dstH, $srcPath);
+      printf($srcPath . "<br>");
+      printf($fileType . "<br>");
     }
+
 //    Dimension neu berechnen
-  function calcDimension($x1, $y1, $x2) {
-    return $y1 * $x2 / $x1;
-}
     //var_dump($imgs);    
   }
 
@@ -78,11 +100,12 @@ class ImgDimension {
     $pattern = sprintf('%s%s.{%s}', $this->srcFolder, $this->srcFileNamePattern, $srcFileTypes);
     return glob($pattern, GLOB_BRACE);
   }
+
   private function getImageFileType($path) {
     $types = ['', 'gif', 'jpeg', 'png'];
     $type = getimagesize($path)[2];
     if ($type > 0 && $type < 4) {
-        return $types[$type];
+      return $types[$type];
     }
     return false;
   }
@@ -93,47 +116,46 @@ class ImgDimension {
     $types = ['', 'gif', 'jpeg', 'png'];
     $type = getimagesize($path)[2];
     if ($type > 0 && $type < 4) {
-        return $types[$type];
+    return $types[$type];
     }
     return false;
-}
+    }
     Neue Datei erstellen
-  * function createResample($srcImg, $srcW, $srcH, $dstW, $dstH, $filetype, $path, $filename) {
+   * function createResample($srcImg, $srcW, $srcH, $dstW, $dstH, $filetype, $path, $filename) {
     $dstPath = false;
     $dstImg = imagecreatetruecolor($dstW, $dstH);
     imagecopyresampled($dstImg, $srcImg, 0, 0, 0, 0, $dstW, $dstH, $srcW, $srcH);
 
     if ($filetype === 2) {
-        $dstPath = $path . $filename . '.jpeg';
-        imagejpeg($dstImg, $dstPath);
+    $dstPath = $path . $filename . '.jpeg';
+    imagejpeg($dstImg, $dstPath);
     } elseif ($filetype === 3) {
-        $dstPath = $path . $filename . '.png';
-        imagepng($dstImg, $dstPath);
+    $dstPath = $path . $filename . '.png';
+    imagepng($dstImg, $dstPath);
     } else {
-        return false;
+    return false;
     }
 
     return $dstPath;
-}   
+    }
     Dimension neu berechnen
    * function calcDimension($x1, $y1, $x2) {
     return $y1 * $x2 / $x1;
-}
+    }
     Namen generieren
    * function getRandName($prefix = '') {
     return str_replace('.', '_', uniqid($prefix, true));
-}
-  
+    }
+
     Speichern der Datei
    * function uploadFile($tmpName, $path, $dstName = false) {
     $n = ($dstName) ? $dstName : getRandName() . '.' . getImageFileType($tmpName);
     if(move_uploaded_file($tmpName, $path . $n)){
-        return $path.$n;
+    return $path.$n;
     }
     return false;
-}
-   **/
-  
+    }
+   * */
   private function checkFiles() {
     
   }
